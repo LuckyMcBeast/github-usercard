0a9d73c3e19c49bd3b4337fe878f91c627aa8886
M. Cullen McClellan <cullenmcclellan@gmail.com>
2020-10-08 20:50:18 -0400

    First API call

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get(" https://api.github.com/users/luckymcbeast")
+  .then(response => {
+    console.log(response)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -49,6 +59,10 @@ const followersArray = [];
       </div>
     </div>
 */
+
+const createCard = (data => {
+  
+});
 
 /*
   List of LS Instructors Github username's:
